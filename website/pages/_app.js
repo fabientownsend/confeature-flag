c662b472e261c3a74f48d2a155caabf1af035dd9
Fabien Townsend <fabientownsend@gmail.com>
2020-05-02 12:18:59 +0100

    Adds initial website

---
+++ website/pages/_app.js
@@ -0,0 +1,6 @@
+import "antd/dist/antd.css"
+import Component from "./index"
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ website/pages/index.js
@@ -0,0 +1,11 @@
+import { Result } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
+
+export default function Index() {
+  return (
+    <Result
+      icon={<SmileOutlined />}
+      title="Welcome to Confeature Flag 🍯"
+    />
+  );
+}
